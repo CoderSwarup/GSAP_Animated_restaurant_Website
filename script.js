84efbd6b7b457903fe6c02164b6fc00ad4b1efc9
Swarup Bhise <swarupbhise420@gmail.com>
2023-12-16 17:37:46 +0530

    Section 3 is complete Main Page

--- script.js
+++ script.js
@@ -162,3 +162,111 @@ document.querySelectorAll("#explore").forEach(function (elem) {
       });
   });
 });
+
+// Change The Page
+
+const example = () => {
+  window.location.href = "/again.html";
+};
+
+// Section 3 Main Page
+
+let t3 = gsap.timeline();
+
+t3.from("#main", {
+  opacity: "0",
+  ease: "expo.inout",
+  duration: 1,
+})
+  .from(".hero-main", {
+    delay: -1,
+    opacity: 0,
+    ease: "expo.inout",
+  })
+  .from(
+    "#line-effect-1",
+    {
+      height: "0%",
+      ease: "expo.inout",
+      duration: 2,
+    },
+    "anim1"
+  )
+  .from(
+    "#line-effect-2 , #line-effect-3",
+    {
+      height: "0%",
+      ease: "expo.inout",
+      duration: 1,
+    },
+    "anim2"
+  )
+  .from(
+    ".img-one",
+    {
+      y: "50%",
+      height: "0%",
+      ease: "expo.inout",
+      duration: 1,
+    },
+    "anim1"
+  )
+  .from(
+    ".mandit",
+    {
+      width: "0%",
+      opacity: 0,
+      ease: "expo.inout",
+      duration: 1,
+    },
+    "anim1"
+  )
+  .from(
+    ".mandit-2",
+    {
+      height: "0%",
+      opacity: 0,
+      ease: "expo.inout",
+      duration: 1,
+    },
+    "anim1"
+  )
+  .from(
+    ".main-text",
+    {
+      opacity: 0,
+      ease: "expo.inout",
+      delay: -0.5,
+      duration: 1,
+    },
+    "anim2"
+  )
+  .from("#nav", {
+    y: -100,
+    duration: 1,
+  });
+
+// Upper Nav
+
+let NavOpen = document.querySelector("#nav-open");
+let upperNav = document.querySelector(".upper-menu");
+let NavClose = document.querySelector("#nav-close");
+NavOpen.addEventListener("click", (e) => {
+  upperNav.style.top = "0";
+});
+
+NavClose.addEventListener("click", (e) => {
+  upperNav.style.top = "-100%";
+});
+
+let UpperMenuLists = document.querySelectorAll(".upper-nav-links");
+let sliderupper = document.querySelector("#slider-upper-wala");
+
+UpperMenuLists.forEach((ele, i) => {
+  ele.addEventListener("mouseover", (e) => {
+    sliderupper.style.transform = `translateY(-${i * 25}%)`;
+  });
+  // ele.addEventListener("mouseout", (e) => {
+  //   sliderupper.style.transform = `translateY(-0%)`;
+  // });
+});
